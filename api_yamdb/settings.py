4f373c5d9455c56063e32c4025e8e7d4037e42d4
Jemand <jemand@MacBook-Pro-Jemand.local>
2021-02-27 20:06:39 +0300

    add settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -25,6 +25,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
+    "178.154.253.78",
+    "yatube-project.ga",
+    "www.yatube-project.ga"
     "127.0.0.1",
     "[::1]",
     "testserver",
